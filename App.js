defd76a957da17156aa3fd9371ec2708a129e810
namdo <kaidn28@gmail.com>
2023-09-06 21:10:53 +0700

    initial

--- App.js
+++ App.js
@@ -1,13 +1,58 @@
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function App() {
+
+const CountComponent = (props) => {
+  console.log(props);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Let's count!</Text>
+      <Text>{props.count} </Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
+};
+
+// export default function App() {
+//   const [count, setCount] = useState(0);
+//   return (
+//     <View style={styles.container}>
+//       <Text>Let's play with props</Text>
+//       <CountComponent count={count} />
+//       <Button
+//         title="Press me"
+//         onPress={()=>setCount(count+1)}
+//       />
+      
+//     </View>
+    
+//   );
+// }
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0
+    }
+  }
+  setCount = () => this.setState({count: this.state.count+1})
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Let's play with props</Text>
+        <CountComponent count={this.state.count} />
+        <Button
+          title="Press me"
+          onPress={()=>this.setCount(this.state.count+1)}
+        />
+        
+      </View>
+      
+    );
+  }
+  
 }
 
 const styles = StyleSheet.create({
